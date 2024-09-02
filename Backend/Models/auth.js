d2a94b38ed0db2aa36b04e50dfa83edeca3bf995
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2024-09-02 16:14:16 +0000

    Trying to connect auth backend with frontend

--- Backend/Models/auth.js
+++ Backend/Models/auth.js
@@ -1,32 +1,32 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        require: true
-    },
-    lastName: {
-        type: String,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    dateOfBirth: {
-        type: Date
-    },
-    verified: {
-        type: Boolean,
-        required: true,
-        default: false
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  verified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
 const User = model("User", userSchema);
 
